cfa5755ad27b5c606ac34d034cda4fad78d58b76
Josh Long <josh@joshlong.com>
2021-08-04 13:04:09 -0700

    gateway hints

---
+++ gateway-hints/src/main/java/com/example/gatewayhints/GatewayHintsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewayhints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayHintsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayHintsApplication.class, args);
+	}
+
+}
---
+++ gateway-hints/src/test/java/com/example/gatewayhints/GatewayHintsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewayhints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayHintsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
